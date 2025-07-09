c1e859e12e8628b07ce5a50aeaadd781f7e30f47
perryjobs <perry.jobs101@gmail.com>
2025-07-08 20:56:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from moviepy.editor 
-import VideoFileClip, concatenate_videoclips, CompositeVideoClip, TextClip
+from moviepy.editor import VideoFileClip, concatenate_videoclips, CompositeVideoClip, TextClip
 import os
 from typing import List
 
